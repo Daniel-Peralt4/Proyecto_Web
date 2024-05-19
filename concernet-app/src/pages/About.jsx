2e2498a11c70327ec3feeb6a10d8a374c2fbfa4b
Daniel-Peralt4 <dperaltapenuela07@gmail.com>
2024-05-18 23:14:50 -0500

    Modificacion y estilizacion del apartado de About

--- concernet-app/src/pages/About.jsx
+++ concernet-app/src/pages/About.jsx
@@ -1,10 +1,20 @@
 import NavBar from "../components/NavBar";
+import '../CSS/About.css';
 
 const About = ()=>{
     return(
         <div className="about">
             <NavBar/>
-            <h1>Sobre nosotros</h1>
+            <div className="contenedorAbout">
+                <h2>MISION</h2>
+                <p>Empoderar a las comunidades para que prosperen a través de soluciones de gestión
+                     innovadoras y eficientes, fomentando un sentido de pertenencia, seguridad
+                      y bienestar para todos los residentes.</p>
+                <h2>VISION</h2>
+                <p>Ser el proveedor líder de soluciones inteligentes de gestión comunitaria, 
+                    transformando las comunidades cerradas en vibrantes centros de conexión, 
+                    colaboración y prosperidad compartida.</p>
+            </div>
         </div>
 
     );
